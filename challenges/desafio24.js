89ccabedce0a9cf4e97508ed70fe5ab061409dc8
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-11 14:36:33 -0300

    REQ24-25-26 - Finalizando Requisito - [DONE]

---
+++ challenges/desafio24.js
@@ -0,0 +1,18 @@
+db.voos.findOne({
+  $and:[
+    {
+      litrosCombustivel: {$exists: true}
+    },
+    {
+      $and:[
+          {
+            "empresa.nome": {$not: {$in:["GOL", "AZUL"]} }
+          },
+          {
+            litrosCombustivel: { $lt: 600}
+          }
+      ]
+    }
+  ]
+},
+{ vooId: true, _id: false, litrosCombustivel: true,  "empresa.nome": true});
