421d48c7c6e4a394d70c622a9f906435545d3ec5
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-10 22:07:29 -0300

    REQ22 - Finalizadno Requisito 22 - [DONE]

---
+++ challenges/desafio22.js
@@ -0,0 +1,13 @@
+db.voos.findOne({
+  $and:[
+    {
+      $or: [{"empresa.nome": "DELTA AIRLINES"}, {"empresa.nome": "AMERICAN AIRLINES"}]
+    },
+    {
+      $and: [ {"aeroportoDestino.sigla": "KJFK"},
+              {"aeroportoOrigem.sigla": "SBGR"}
+            ]
+    }
+  ]
+},
+{ vooId: true, _id: false });
